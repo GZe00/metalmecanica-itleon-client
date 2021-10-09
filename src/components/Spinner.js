ac0c6f9c840e9104c06f3013aa87a6003785c2f4
Moises Fuentes <dev.gze.moises@gmail.com>
2021-10-09 05:31:57 -0500

    Spinner anadido

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,32 @@
+import { CgSpinnerTwo } from "react-icons/cg";
+import styled from "styled-components";
 
+const SpinnerStyled = styled.div`
+  .spinner {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+  }
 
-export function Spinner() {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  .spinning {
+    animation: spinner 1s linear infinite;
+  }
+
+  @keyframes spinner {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Spinner = () => {
+  return (
+    <SpinnerStyled>
+      <div className="spinner">
+        <CgSpinnerTwo className="spinning" size={60} style={{color: '#01135D'}} />
+      </div>
+    </SpinnerStyled>
+  );
+};
+
+export default Spinner;
